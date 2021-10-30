b3c6042abe1d985e18def6e7b71e1d0924c7dabc
Nathalia Rus <nathaliarus@gmail.com>
2021-10-30 17:26:45 +0100

    basic setup for todos mvc react

--- todos-app/src/App.tsx
+++ todos-app/src/App.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Todos, TodoType } from "react-todomvc";
+import "react-todomvc/dist/todomvc.css";
+
+// GraphQL constants
 
 function App() {
+  // Get data etc
+
+  const todos: [] = [];
+
+  const addNewTodo = async (value: string) => {};
+  const updateTodo = async (value: TodoType) => {};
+  const deleteTodo = async (id: string) => {};
+  const clearCompletedTodos = async () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Todos
+        todos={[...todos]}
+        addNewTodo={addNewTodo}
+        updateTodo={updateTodo}
+        deleteTodo={deleteTodo}
+        clearCompletedTodos={clearCompletedTodos}
+      />
+    </>
   );
 }
 
