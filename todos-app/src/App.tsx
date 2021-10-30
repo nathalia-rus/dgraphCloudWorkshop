4827377ecee879a86440bfaeebe8b8721320cafe
Nathalia Rus <nathaliarus@gmail.com>
2021-10-30 17:58:28 +0100

    query

--- todos-app/src/App.tsx
+++ todos-app/src/App.tsx
@@ -1,23 +1,28 @@
 import "./App.css";
 import { Todos, TodoType } from "react-todomvc";
 import "react-todomvc/dist/todomvc.css";
+import { GET_TODOS } from "./graphql/query";
+import { useQuery } from "@apollo/client";
 
 // GraphQL constants
 
 function App() {
   // Get data etc
 
-  const todos: [] = [];
+  const { loading, error, data } = useQuery(GET_TODOS);
 
   const addNewTodo = async (value: string) => {};
   const updateTodo = async (value: TodoType) => {};
   const deleteTodo = async (id: string) => {};
   const clearCompletedTodos = async () => {};
 
+  if (loading) return <p>Loading todos...</p>;
+  if (error) return <p>`Error: ${data.message}`</p>;
+
   return (
     <>
       <Todos
-        todos={[...todos]}
+        todos={[...data.queryTodo]}
         addNewTodo={addNewTodo}
         updateTodo={updateTodo}
         deleteTodo={deleteTodo}
